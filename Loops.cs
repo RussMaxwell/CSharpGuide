fa10bb8d88ce159f0febdadcf61fdde5c9dbd72d
RussMaxwell <52090077+RussMaxwell@users.noreply.github.com>
2024-02-22 17:21:51 -0500

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,66 @@
+﻿////////////
+//For Loop//
+////////////
+Console.WriteLine("Printing Numbers");
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+Console.WriteLine();
+
+
+
+/////////////////
+//For Each Loop//
+/////////////////
+Console.WriteLine("Printing Names");
+string[] names = { "John", "Rachel", "Brent" , "Scott"};
+
+foreach (string nam in names)
+{
+    Console.WriteLine(nam);
+}
+
+
+//////////////
+//While Loop//
+//////////////
+int mynum = 0;
+while (mynum != 5)
+{
+    Console.WriteLine("Enter any number other than 5:");
+    var input = Console.ReadLine();
+    bool anum = int.TryParse(input, out mynum);
+
+    if (anum && mynum != 5)
+    {
+        Console.WriteLine($"You entered {mynum}");
+    }
+
+    else if (anum && mynum == 5)
+    {
+        Console.WriteLine("You entered 5, exiting out of while loop");
+    }
+    else
+    {
+        Console.WriteLine("Invalid number, try again");
+    }
+ }
+
+
+
+
+/////////////////
+//Do While Loop//
+/////////////////
+
+string input2;
+
+do
+{ 
+    Console.WriteLine("Enter anything to keep do while loop alive:");
+    input2 = Console.ReadLine(); 
+} while (!string.IsNullOrEmpty(input2));
+
