ec904b7cec867af4b0ca2d4ee00f206944c3453f
RussMaxwell <52090077+RussMaxwell@users.noreply.github.com>
2024-02-22 15:40:00 -0500

    Add files via upload

---
+++ OOP-Basic/Animals/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace Animals
+{
+    //Dog must be set as public to be used in other projects
+    public class Dog
+    {
+        string Name { get; set; }
+        string Breed { get; set; }
+        string Age { get; set; }
+
+        public Dog(string tmpname, string tmpbreed, string tmpage)
+        {
+            this.Name = tmpname;
+            this.Breed = tmpbreed;
+            this.Age = tmpage;
+        }
+
+        //public so it can be called outside of project 
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} goes roof roof!");
+            Details();
+        }
+
+        //only want this class to execute this function
+        internal void Details()
+        {
+            Console.WriteLine($"{Name} is a {Breed} and {Age} years old!");
+        }
+
+    }
+}
---
+++ OOP-Basic/DemoClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using Animals;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog mydog = new Dog("rex", "bulldog", "5");
+        mydog.Bark();
+
+
+        Dog myotherdog = new("buddy", "terrior", "18");
+        myotherdog.Bark();
+    }   
+}
+
+
+
+
