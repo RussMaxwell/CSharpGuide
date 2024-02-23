a888c7b411cd6589db9f0a0c9b8a6e59d5a3ce55
RussMaxwell <52090077+RussMaxwell@users.noreply.github.com>
2024-02-22 19:19:49 -0500

    Add files via upload

---
+++ ternary.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your Age");
+
+        var age = Console.ReadLine();
+
+        if (int.TryParse(age, out int aged))
+        {
+            var hmm = aged > 40 ? "You are old" : "You are young";
+            Console.WriteLine(hmm);
+
+        }
+
+    }
+}
