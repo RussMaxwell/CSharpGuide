8c5e7ab9c03f250cd88925693f7ed27e624d170b
RussMaxwell <52090077+RussMaxwell@users.noreply.github.com>
2024-02-23 14:26:27 -0500

    Add files via upload

---
+++ operators/null-coalescing.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //null coalescing example 1
+        string name = null;
+        var result = name ?? "No Name";
+        Console.WriteLine(result);
+
+        //null coalescing example 2
+        string name2 = "John";
+        var result2 = name2 ?? "No Name";
+        Console.WriteLine(result2);
+    }
+}
